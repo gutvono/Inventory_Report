10532dc8d9e49b729e551def6db2ffb65e708d00
Gustavo Vono <guvono4@gmail.com>
2023-03-03 18:34:33 -0300

    Req 1 OK

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id, ndp, nde, ddf, ddv, nds, ida = [
+        1,
+        "Radeon RX 6650 XT 8GB",
+        "Sapphire AMD",
+        "10/5/2022",
+        "Vitalício",
+        "Nitro X",
+        "Local seguro"
+    ]
+
+    product = Product(id, ndp, nde, ddf, ddv, nds, ida)
+
+    assert product.id == id
+    assert product.nome_do_produto == ndp
+    assert product.nome_da_empresa == nde
+    assert product.data_de_fabricacao == ddf
+    assert product.data_de_validade == ddv
+    assert product.numero_de_serie == nds
+    assert product.instrucoes_de_armazenamento == ida
