de9d7ae8e7a0427bb9dc822d2890e955c540d266
Gustavo Vono <guvono4@gmail.com>
2023-03-06 16:57:29 -0300

    req 2 ok

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,31 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data: list):
+        oldest_manufacturing_date = str(date.max)
+        nearest_expiration_date = str(date.max)
+        today = str(date.today())
+        counter = {}
+
+        for product in data:
+            if product["data_de_fabricacao"] < oldest_manufacturing_date:
+                oldest_manufacturing_date = product["data_de_fabricacao"]
+
+            if today <= product["data_de_validade"] < nearest_expiration_date:
+                nearest_expiration_date = product["data_de_validade"]
+
+            if product["nome_da_empresa"] in counter:
+                counter[product["nome_da_empresa"]] += 1
+            else:
+                counter[product["nome_da_empresa"]] = 1
+
+        most_commom_company = max(counter, key=counter.get)
+        company_with_more_products = most_commom_company
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {nearest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
