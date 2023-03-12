e0c8c5bdb9feaf4697dd5391a08b9e8d6ba1c549
Gustavo Vono <guvono4@gmail.com>
2023-03-12 12:23:05 -0300

    req 3 ok

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
+from inventory_report.reports.simple_report import SimpleReport
+from typing import List, Dict
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(data: List[Dict]) -> str:
+        simple_report = SimpleReport.generate(data)
+        products_by_companies = {}
+
+        for product in data:
+            if product["nome_da_empresa"] in products_by_companies:
+                products_by_companies[product["nome_da_empresa"]] += 1
+            else:
+                products_by_companies[product["nome_da_empresa"]] = 1
+
+        companies = "\nProdutos estocados por empresa:\n"
+        for company, quantity in products_by_companies.items():
+            companies += f"- {company}: {quantity}\n"
+
+        return (
+            f"{simple_report}"
+            f"{companies}"
+        )
